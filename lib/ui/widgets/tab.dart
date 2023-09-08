332ad18a3fe4ddfd1b953b0803018f0393bd2fb4
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-07 19:26:11 -0600

    feat: conver tab to Button

--- lib/ui/widgets/tab.dart
+++ lib/ui/widgets/tab.dart
@@ -5,39 +5,28 @@ class TabWidget extends StatelessWidget {
     super.key,
     required this.lable,
     this.onPressed,
-    this.simple = true,
   });
 
   final String lable;
   final VoidCallback? onPressed;
-  final bool simple;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 8.0,
-        horizontal: 15.0,
-      ),
-      child: Row(
-        children: [
-          Text(
-            lable,
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
+    final size = MediaQuery.of(context).size;
+    return InkWell(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 8.0,
+          horizontal: 15.0,
+        ),
+        child: Text(
+          lable,
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+            fontSize: size.width > 800 ? 20 : 25,
           ),
-          if (!simple)
-            IconButton(
-              onPressed: onPressed,
-              icon: Icon(
-                Icons.keyboard_arrow_down_outlined,
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
-              hoverColor: Colors.blue[100],
-              splashRadius: 15.0,
-            ),
-        ],
+        ),
       ),
     );
   }
