9af66705d017a875c389c44c1aeeac1bea5e9e8b
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-07 19:23:57 -0600

    feat: make responsive the appbar

--- lib/ui/widgets/appbar.dart
+++ lib/ui/widgets/appbar.dart
@@ -5,7 +5,10 @@ import 'package:unihacks_ui_kit/buttons/theme_swtcher.dart';
 class UNIHacksAppBar extends StatelessWidget implements PreferredSizeWidget {
   const UNIHacksAppBar({
     super.key,
+    this.scrollController,
   });
+
+  final ScrollController? scrollController;
 
   @override
   Size get preferredSize => const Size.fromHeight(100);
@@ -13,7 +16,6 @@ class UNIHacksAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    print(size.aspectRatio);
     return AppBar(
       backgroundColor: Theme.of(context).colorScheme.primary,
       toolbarHeight: 150,
@@ -41,33 +43,53 @@ class UNIHacksAppBar extends StatelessWidget implements PreferredSizeWidget {
               height: 60,
             ),
           ),
-          Text(
-            'UNIHacks',
-            style: TextStyle(
-              color: Colors.blue[700],
-              fontSize: 25.0,
+          if (size.width > 850)
+            Text(
+              'UNIHacks',
+              style: TextStyle(
+                color: Colors.blue[700],
+                fontSize: 25.0,
+              ),
             ),
-          ),
           const SizedBox(
             width: 20,
           ),
-          TabWidget(
-            lable: 'Projects',
-            onPressed: () {},
-            simple: false,
-          ),
-          TabWidget(
-            lable: 'About',
-            onPressed: () {},
-          ),
-          TabWidget(
-            lable: 'Developers',
-            onPressed: () {},
-          ),
-          TabWidget(
-            lable: 'Contact',
-            onPressed: () {},
-          ),
+          if (size.width > 700)
+            TabWidget(
+              lable: 'Projects',
+              onPressed: () {
+                scrollController?.animateTo(250,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.decelerate);
+              },
+            ),
+          if (size.width > 700)
+            TabWidget(
+              lable: 'About',
+              onPressed: () {
+                scrollController?.animateTo(0,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.decelerate);
+              },
+            ),
+          if (size.width > 700)
+            TabWidget(
+              lable: 'Developers',
+              onPressed: () {
+                scrollController?.animateTo(800,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.decelerate);
+              },
+            ),
+          if (size.width > 700)
+            TabWidget(
+              lable: 'Contact',
+              onPressed: () {
+                scrollController?.animateTo(1500,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.decelerate);
+              },
+            ),
         ],
       ),
     );
